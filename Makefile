8d37eec21f4e4b5e2049a548874f51cc8951b5e8
Mikel Egaña Aranguren <mikel.egana.aranguren@gmail.com>
2021-12-06 22:55:30 +0100

    first commit

---
+++ Makefile
@@ -0,0 +1,2 @@
+verify: 
+	robot verify --input robot-tests-pass.owl --queries verify-label.rq --output-dir report
---
+++ robot.sh
@@ -0,0 +1,4 @@
+# robot verify --input robot-tests.owl --queries verify-label.rq --output-dir report
+# robot verify --input robot-tests-pass.owl --queries verify-label.rq --output-dir report
+
+# robot annotate --input robot-tests.owl --version-iri "http://ehu.eus/robot-tests/0.0.1" --output annotated.owl
